ed358c882be53c452e2080e2828c7362613df1d0
Matias Sørensen <sorensen.matias@gmail.com>
2024-02-15 22:40:40 +0100

    Move ExtendedValidationServiceProviderFactory to separate class

--- ServiceProviderValidationExtensions/Class1.cs
+++ ServiceProviderValidationExtensions/Class1.cs
@@ -3,28 +3,6 @@ using Microsoft.Extensions.Hosting;
 
 namespace ServiceProviderValidationExtensions
 {
-    internal sealed class ExtendedValidationServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
-    {
-        private readonly DefaultServiceProviderFactory _defaultServiceProviderFactory;
-
-        public ExtendedValidationServiceProviderFactory(DefaultServiceProviderFactory defaultServiceProviderFactory)
-        {
-            _defaultServiceProviderFactory = defaultServiceProviderFactory;
-        }
-
-        public IServiceCollection CreateBuilder(IServiceCollection services)
-        {
-            return _defaultServiceProviderFactory.CreateBuilder(services);
-        }
-
-        public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder)
-        {
-            var serviceProvider = _defaultServiceProviderFactory.CreateServiceProvider(containerBuilder);
-            serviceProvider.Validate();
-            return serviceProvider;
-        }
-    }
-
     public static class Ext
     {
         public static IServiceCollection AddSingletonExclusive<TService>(
---
+++ ServiceProviderValidationExtensions/ExtendedValidationServiceProviderFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace ServiceProviderValidationExtensions;
+
+internal sealed class ExtendedValidationServiceProviderFactory : IServiceProviderFactory<IServiceCollection>
+{
+    private readonly DefaultServiceProviderFactory _defaultServiceProviderFactory;
+
+    public ExtendedValidationServiceProviderFactory(DefaultServiceProviderFactory defaultServiceProviderFactory)
+    {
+        _defaultServiceProviderFactory = defaultServiceProviderFactory;
+    }
+
+    public IServiceCollection CreateBuilder(IServiceCollection services)
+    {
+        return _defaultServiceProviderFactory.CreateBuilder(services);
+    }
+
+    public IServiceProvider CreateServiceProvider(IServiceCollection containerBuilder)
+    {
+        var serviceProvider = _defaultServiceProviderFactory.CreateServiceProvider(containerBuilder);
+        serviceProvider.Validate();
+        return serviceProvider;
+    }
+}
